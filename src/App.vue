7a6b029c95485cbab336529c2ae85fa24a08f0fd
pete <peter.reiter@cohu.com>
2020-08-12 14:05:34 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {
+  API
+} from 'aws-amplify';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    getPrograms() {
+      const apiName = 'keyserverAPI';
+      const path = '/program';
+      const myInit = { // OPTIONAL
+        // headers: {}, // OPTIONAL
+        // response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+        // queryStringParameters: {  // OPTIONAL
+        //     name: 'param',
+        // },
+      };
+
+      API
+        .get(apiName, path, myInit)
+        .then(response => {
+          console.log('program response');
+          console.log(response);
+        })
+        .catch(error => {
+          console.log('error response from api');
+          console.log(error.response);
+        });
+    }
+  },
+  mounted: function() {
+    this.getPrograms();
   }
 }
 </script>
